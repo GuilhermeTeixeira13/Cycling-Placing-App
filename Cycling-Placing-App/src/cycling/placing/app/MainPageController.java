60cb84a4550bb9132d2eca9e7f4df964249763b7
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2022-06-20 22:15:32 +0100

    Added the first needed files.
    NetBeans Project, Main.java, MainPageController.java, MainPage.fxml

---
+++ Cycling-Placing-App/src/cycling/placing/app/MainPageController.java
@@ -0,0 +1,13 @@
+package cycling.placing.app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class MainPageController implements Initializable {    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
